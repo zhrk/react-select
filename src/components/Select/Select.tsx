2e77956cba7a58f2a163bade0a761aa7df9b9b4c
zhrk <zshchipets@gmail.com>
2022-01-28 00:31:41 +0300

    added keyboard support for options navigation

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,6 @@
+import { useRef } from 'react';
 import styles from './styles.module.scss';
-import { Options, Value } from './types';
+import { Options, Value, SiblingElement } from './types';
 import useSelect from './useSelect';
 
 interface Props {
@@ -10,8 +11,28 @@ interface Props {
 const Select = (props: Props) => {
   const { ...config } = props;
 
+  const ref = useRef<HTMLDivElement>(null);
+
   const { options, search, visible, selectOption, clearValue, onInputChange, onInputFocus } =
     useSelect(config);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (ref.current) {
+      const elements = Array.from(ref.current.children) as Array<HTMLButtonElement>;
+
+      const firstElement = elements[0];
+      const lastElement = elements[elements.length - 1];
+      const focusedElement = document.activeElement;
+
+      const prevOption = focusedElement?.previousSibling as SiblingElement;
+      const nextOption = focusedElement?.nextSibling as SiblingElement;
+
+      if (event.key === 'ArrowUp') prevOption?.focus();
+      if (event.key === 'ArrowDown') nextOption?.focus();
+      if (event.key === 'Home') firstElement?.focus();
+      if (event.key === 'End') lastElement?.focus();
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -22,9 +43,27 @@ const Select = (props: Props) => {
         </button>
       </div>
       {visible && (
-        <div className={styles.options}>
+        <div
+          ref={ref}
+          role="listbox"
+          tabIndex={-1}
+          className={styles.options}
+          onKeyDown={handleKeyDown}
+        >
           {options.map((option) => (
-            <button key={option.value} type="button" onClick={() => selectOption(option)}>
+            <button
+              key={option.value}
+              type="button"
+              role="option"
+              aria-selected="false"
+              onClick={() => selectOption(option)}
+              onFocus={(event) => {
+                event.target.ariaSelected = 'true';
+              }}
+              onBlur={(event) => {
+                event.target.ariaSelected = 'false';
+              }}
+            >
               {option.label}
             </button>
           ))}
--- src/components/Select/types.ts
+++ src/components/Select/types.ts
@@ -1,3 +1,5 @@
 export type Option = { value: string; label: string };
 export type Options = Option[];
 export type Value = Option | null;
+
+export type SiblingElement = HTMLButtonElement | null;
