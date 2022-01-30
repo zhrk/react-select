3e6c3f6420c3552b80812a8fa7c81813dc5560cb
zhrk <zshchipets@gmail.com>
2022-01-30 23:13:25 +0300

    move options & option logic to hook

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,6 +1,5 @@
-import { useRef } from 'react';
 import styles from './styles.module.scss';
-import { Options, Value, SiblingElement } from './types';
+import { Options, Value } from './types';
 import useSelect from './useSelect';
 
 interface Props {
@@ -11,28 +10,17 @@ interface Props {
 const Select = (props: Props) => {
   const { ...config } = props;
 
-  const ref = useRef<HTMLDivElement>(null);
-
-  const { options, search, visible, selectOption, clearValue, onInputChange, onInputFocus } =
-    useSelect(config);
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (ref.current) {
-      const elements = Array.from(ref.current.children) as Array<HTMLButtonElement>;
-
-      const firstElement = elements[0];
-      const lastElement = elements[elements.length - 1];
-      const focusedElement = document.activeElement;
-
-      const prevOption = focusedElement?.previousSibling as SiblingElement;
-      const nextOption = focusedElement?.nextSibling as SiblingElement;
-
-      if (event.key === 'ArrowUp') prevOption?.focus();
-      if (event.key === 'ArrowDown') nextOption?.focus();
-      if (event.key === 'Home') firstElement?.focus();
-      if (event.key === 'End') lastElement?.focus();
-    }
-  };
+  const {
+    options,
+    search,
+    visible,
+    selectOption,
+    clearValue,
+    onInputChange,
+    onInputFocus,
+    optionProps,
+    optionsProps,
+  } = useSelect(config);
 
   return (
     <div className={styles.container}>
@@ -43,26 +31,13 @@ const Select = (props: Props) => {
         </button>
       </div>
       {visible && (
-        <div
-          ref={ref}
-          role="listbox"
-          tabIndex={-1}
-          className={styles.options}
-          onKeyDown={handleKeyDown}
-        >
+        <div className={styles.options} {...optionsProps}>
           {options.map((option) => (
             <button
               key={option.value}
               type="button"
-              role="option"
-              aria-selected="false"
               onClick={() => selectOption(option)}
-              onFocus={(event) => {
-                event.target.ariaSelected = 'true';
-              }}
-              onBlur={(event) => {
-                event.target.ariaSelected = 'false';
-              }}
+              {...optionProps}
             >
               {option.label}
             </button>
--- src/components/Select/useSelect.ts
+++ src/components/Select/useSelect.ts
@@ -1,6 +1,6 @@
 import { matchSorter } from 'match-sorter';
-import { useState, useEffect } from 'react';
-import { Options, Option, Value } from './types';
+import { useState, useEffect, useRef } from 'react';
+import { Options, Option, Value, SiblingElement } from './types';
 
 interface Config {
   options: Options;
@@ -36,6 +36,26 @@ const Select = (config: Config) => {
     }
   };
 
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (ref.current) {
+      const elements = Array.from(ref.current.children) as Array<HTMLButtonElement>;
+
+      const firstElement = elements[0];
+      const lastElement = elements[elements.length - 1];
+      const focusedElement = document.activeElement;
+
+      const prevOption = focusedElement?.previousSibling as SiblingElement;
+      const nextOption = focusedElement?.nextSibling as SiblingElement;
+
+      if (event.key === 'ArrowUp') prevOption?.focus();
+      if (event.key === 'ArrowDown') nextOption?.focus();
+      if (event.key === 'Home') firstElement?.focus();
+      if (event.key === 'End') lastElement?.focus();
+    }
+  };
+
   return {
     visible,
     options: search ? matchSorter(options, search, { keys: ['label'] }) : options,
@@ -44,6 +64,22 @@ const Select = (config: Config) => {
     onInputFocus: () => setVisible(true), // вынести всё что относится к инпуту в отдельный объект
     clearValue,
     selectOption,
+    optionsProps: {
+      ref,
+      role: 'listbox',
+      tabIndex: -1,
+      onKeyDown: handleKeyDown,
+    },
+    optionProps: {
+      role: 'option',
+      'aria-selected': false,
+      onFocus: (event: React.FocusEvent) => {
+        event.target.ariaSelected = 'true';
+      },
+      onBlur: (event: React.FocusEvent) => {
+        event.target.ariaSelected = 'false';
+      },
+    },
   };
 };
 
