2e410526d3b07e9a3f8018c60831545c13519bc4
zhrk <zshchipets@gmail.com>
2022-02-24 20:28:39 +0300

    removed as castings, added type guards

--- src/components/Select/types.ts
+++ src/components/Select/types.ts
@@ -1,5 +1,3 @@
 export type Option = { value: string | number; label: string };
 export type Options = Option[];
 export type Value = Option | null;
-
-export type SiblingElement = HTMLButtonElement | null;
--- src/components/Select/useSelectAccessibility.ts
+++ src/components/Select/useSelectAccessibility.ts
@@ -1,5 +1,18 @@
 import { useRef } from 'react';
-import { SiblingElement } from './types';
+
+const isButtonElement = (element: unknown): element is HTMLButtonElement => {
+  if (element instanceof HTMLButtonElement) return true;
+
+  return false;
+};
+
+const isButtonElements = (elements: unknown): elements is Array<HTMLButtonElement> => {
+  if (elements instanceof HTMLCollection) {
+    if (Array.from(elements).every((element) => element instanceof HTMLButtonElement)) return true;
+  }
+
+  return false;
+};
 
 const useSelectAccessibility = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -7,22 +20,27 @@ const useSelectAccessibility = () => {
   const getOptionsElements = () => {
     const element = ref.current;
 
-    if (element) {
-      const elements = Array.from(element.children) as Array<HTMLButtonElement>;
+    if (element && isButtonElements(element.children)) {
+      const elements = Array.from(element.children);
 
       const firstElement = elements[0];
       const lastElement = elements[elements.length - 1];
       const focusedElement = document.activeElement;
 
-      const prevOption = focusedElement?.previousSibling as SiblingElement;
-      const nextOption = focusedElement?.nextSibling as SiblingElement;
+      const prevOption = focusedElement?.previousSibling;
+      const nextOption = focusedElement?.nextSibling;
 
-      return {
-        firstElement,
-        lastElement,
-        prevOption,
-        nextOption,
-      };
+      if (isButtonElement(prevOption) && isButtonElement(nextOption)) {
+        return { firstElement, lastElement, prevOption, nextOption };
+      }
+
+      if (isButtonElement(prevOption)) {
+        return { firstElement, lastElement, prevOption };
+      }
+
+      if (isButtonElement(nextOption)) {
+        return { firstElement, lastElement, nextOption };
+      }
     }
 
     return {};
