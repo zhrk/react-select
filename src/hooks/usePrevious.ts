74cc89c532c131114f012faee007eedf8f9d2885
zhrk <zshchipets@gmail.com>
2022-04-21 01:22:02 +0300

    fixed usePrevious hook

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 
 const usePrevious = <T>(value: T) => {
-  const ref = useRef<T>(value);
+  const ref = useRef<T | null>(value);
 
   useEffect(() => {
     ref.current = value;
